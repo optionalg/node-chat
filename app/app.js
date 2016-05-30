60fa4c9bf7302ab8810236d1f502f6a2a1674345
Plancake <services@plancake.com>
2016-05-30 20:35:19 +0100

    added logging and error reporting

--- app/app.js
+++ app/app.js
@@ -1,20 +1,69 @@
 var express = require('express');
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var rootRoutes = require('./routes/index');
+
 var app = express();
 
 var config = require('config');
 var serverConfig = config.get('server');
-var serverPortConfig = parseInt(serverConfig.port);
+var serverPortConfig = parseInt(serverConfig.port, 10);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', rootRoutes);
+
 app.get('/', function (req, res) {
-  res.render('index', { title : 'Home' });
+  res.render('index', { title : 'Node Chat' });
 });
 
-app.get('/about', function (req, res) {
-  res.send('About page');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
 
 app.listen(serverPortConfig, serverConfig.ip, function () {
   console.log('Chat app listening on port ' + serverPortConfig);
